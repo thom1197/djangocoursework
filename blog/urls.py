b7bf992a7b83720b8d18eb48e4923f828206d450
Thomas Davies <thom1197@hotmail.com>
2020-08-28 23:27:11 +0100

    Change post_detail view to work on pythonanywhere

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('cv', views.cv_view, name='cv'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('r^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     path('contact', views.contact_view, name='contact'),
 ]
 
